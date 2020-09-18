30609b64265f051d668d3b61e1a1666393308caf
99002486 <71490592+99002486@users.noreply.github.com>
2020-09-18 16:07:46 +0530

    Update and rename primeNumber.c to src/primeNumber.c

--- src/primeNumber.c
+++ src/primeNumber.c
@@ -1,12 +1,12 @@
 #include "primeNumber.h"
 
-int primeNumber(int n)
+int primeNumber(int number)
 {
-    if (n>1)                     //proceeds only if number is greater than 1
+    if (number>1)                     //proceeds only if number is greater than 1
     {
-        if (n==2 || n==3)        //returns 1 for 2 or 3
+        if (number==2 || number==3)        //returns 1 for 2 or 3
             return 1;
-        else if (n%2==0)         //eliminates even numbers
+        else if (number%2==0)         //eliminates even numbers
             return -1;
         else
         {
@@ -15,7 +15,7 @@ int primeNumber(int n)
             If there are none, it is prime*/
             for(int i=3;i<n;i+2)
             {
-                if(n%i==0)
+                if(number%i==0)
                     return -1;
                 else
                     return 1;
