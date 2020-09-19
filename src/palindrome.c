22325df882681a225b189f7e1ddd06f6fba5d00c
99002486 <71490592+99002486@users.noreply.github.com>
2020-09-19 13:58:08 +0530

    Update palindrome.c

--- src/palindrome.c
+++ src/palindrome.c
@@ -2,13 +2,13 @@
 
 int palindrome(int number)
 {
-	int reversed_number = 0, remainder, original_number;
+	int reversed_number = 0, original_number;
     	original_number = number;
 
     	// reversed integer is stored in reversed_number
     	while (number != 0) 
 	{
-        	remainder = number % 10;
+        	int remainder = number % 10;
         	reversed_number = (reversed_number * 10) + remainder;
         	number /= 10;
     	}
